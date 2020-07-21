7019994de00da163d7ff0d0559b45d454840324d
NileshKapadia <nileshkapadiya04@gmail.com>
2020-07-21 15:50:04 -0400

    seed data

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -3,10 +3,18 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Model\Review;
+
 use Faker\Generator as Faker;
 
 $factory->define(Review::class, function (Faker $faker) {
     return [
         //
+        'product_id'=>function(){
+            return Product::all()->random();
+        },
+        'customer' =>$faker->word,
+        'review' =>$faker->paragraph,
+        'star' =>$faker->numberBetween(2,30)
+        
     ];
 });
