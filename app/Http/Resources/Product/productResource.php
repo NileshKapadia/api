b9d546ecedac5b5e8abf3954e6f19f1c2c966484
NileshKapadia <nileshkapadiya04@gmail.com>
2020-07-21 17:36:36 -0400

    created new fields

--- app/Http/Resources/Product/productResource.php
+++ app/Http/Resources/Product/productResource.php
@@ -18,12 +18,15 @@ class productResource extends JsonResource
             'name' => $this->name,
             'description' => $this->details,
             'price' => $this->price,
-            'stock' => $this->stock,
+            'stock' => $this->stock == 0 ? 'out of stock': $this->stock,
             'discount' => $this->discount,
+            'totalPrice' => round((1-($this->discount/100)) * $this->price ,2),
+            'rating'=>$this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count()) : 'No Rating yet',
             
             'href'=> [
                 'reviews'=> route('reviews.index',$this->id)
             ]
+            //http://localhost:8000/api/products/4/reviews
         ];
     }
 }
